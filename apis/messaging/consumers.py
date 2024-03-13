73ce90d6fc898f45311f7b3401f8019e1b2e40b7
mbiacharetyron <mbiacharetyron.com>
2024-03-13 10:45:46 +0100

    Changed permissions on group messaging.

--- apis/messaging/consumers.py
+++ apis/messaging/consumers.py
@@ -22,21 +22,22 @@ class GroupChatConsumer(SyncConsumer):
     
     def websocket_connect(self, event): 
         try:
-            usr = self.scope['url_route']['kwargs']['user_id']
+            # usr = self.scope['url_route']['kwargs']['user_id']
             group_id = self.scope['url_route']['kwargs']['group_id']
-            
+            user = self.scope["user"]
             # user = self.get_user(usr)
-            # print(user)
+            print(user)
+            
+            # Check if the user is a member of the group
+            if not self.group_has_user(group_id, user.id):
+                print(f"User {user.id} is not a member of the group.")
+                raise DenyConnection("You are not a member of this group.")
+            
             self.group_name = f"group_{group_id}"
             print(self.group_name)
             
             try:
-                user = User.objects.get(id=usr, is_deleted=False)
                 group = ChatGroup.objects.get(id=group_id)
-            except User.DoesNotExist:
-                print(f"User with id={usr} not found or is deleted.")
-                # async_to_sync(self.close)()
-                raise DenyConnection("User not found or is deleted")
             except ChatGroup.DoesNotExist:
                 print(f"ChatGroup with id={group_id} not found or is deleted.")
                 raise DenyConnection("User not found or is deleted")
@@ -113,6 +114,14 @@ class GroupChatConsumer(SyncConsumer):
         except Exception as e:
             print("An error occured:", str(e))
     
+    
+    def group_has_user(group_id, user_id):
+        try:
+            # Check if the user is a member of the group
+            group = ChatGroup.objects.get(id=group_id, members__id=user_id)
+            return True
+        except ChatGroup.DoesNotExist:
+            return False
 
 
 class ChatConsumer(SyncConsumer):
@@ -241,57 +250,3 @@ class ChatConsumer(SyncConsumer):
             return group_name
 
 
-
-# class ChatConsumer(AsyncWebsocketConsumer):
-#     # def websocket__
-#     async def connect(self): 
-#         # me = self.scope['user']
-#         usr = self.scope['url_route']['kwargs']['user_id']
-#         other_usr = self.scope['url_route']['kwargs']['other_user']
-        
-#         user = User.objects.get(id=usr, is_deleted=False)
-#         other_user = User.objects.get(id=other_usr, is_deleted=False)
-        
-#         self.group_name = f"thread_{other_user.id}-{user}"
-#         print(f'[{self.channel_name}] - You are connected')
-        
-#         await self.channel_layer.group_add(self.group_name, self.channel_name)
-#         await self.accept()
-        
-#     async def disconnect(self, close_code):
-#         await self.channel_layer.group_discard(self.group_name, self.channel_name)
-
-#     async def receive(self, text_data):
-#         text_data_json = json.loads(text_data)
-#         message = text_data_json
-#         print(message)
-
-#         event = {
-#             'type': 'send_message',
-#             'message': message,
-#         }
-
-#         await self.channel_layer.group_send(self.group_name, event)
-
-#     async def send_message(self, event):
-
-#         data = event['message']
-#         await self.create_message(data=data)
-
-#         response_data = {
-#             'sender': data['sender'],
-#             'message': data['message']
-#         }
-#         await self.send(text_data=json.dumps({'message': response_data}))
-
-#     @database_sync_to_async
-#     def create_message(self, data):
-
-#         get_room_by_name = ChatGroup.objects.get(name=data['group_name'])
-        
-#         if not DirectMessage.objects.filter(content=data['message']).exists():
-#             new_message = DirectMessage(group=get_room_by_name, sender=data['sender'], content=data['message'])
-#             new_message.save()  
- 
- 
-        
