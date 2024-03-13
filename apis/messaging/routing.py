2433dfdc77dd8374da13a078ddce5708345811a3
mbiacharetyron <mbiacharetyron.com>
2024-03-13 09:17:54 +0100

    Changed route for group messaging.

--- apis/messaging/routing.py
+++ apis/messaging/routing.py
@@ -6,5 +6,5 @@ from .consumers import ChatConsumer, GroupChatConsumer
 websocket_urlpatterns = [
     # path('ws/chat/<str:other_user>/?token=<str:token>/', ChatConsumer.as_asgi()),
     path('ws/chat/<str:other_user>/', ChatConsumer.as_asgi()),
-    path('ws/group/<str:group_id>/<str:user_id>/', GroupChatConsumer.as_asgi()),
+    path('ws/group/<str:group_id>/', GroupChatConsumer.as_asgi()),
 ]
