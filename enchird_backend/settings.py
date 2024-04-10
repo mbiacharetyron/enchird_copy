e0f70efd9d49641acf9aee4f16bd351e55701ba1
mbiacharetyron <mbiacharetyron.com>
2024-04-10 09:02:03 +0100

    Settings file changed v2

--- enchird_backend/settings.py
+++ enchird_backend/settings.py
@@ -323,7 +323,7 @@ CORS_ALLOWED_ORIGINS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
